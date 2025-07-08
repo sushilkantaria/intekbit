834d199a3f0d3b1dab6013709c6012074e596cb2
Banti377 <bantipatel2506@gmail.com>
2025-07-08 18:28:08 +0530

    add changes

--- src/pages/CloudServices.jsx
+++ src/pages/CloudServices.jsx
@@ -4,14 +4,12 @@ import cloudServices from '../data/cloudServices';
 
 const CloudServices = () => {
 	return (
-		<div className="bg-white min-h-screen text-gray-900">
 			<ServiceTabs
 				servicesData={cloudServices}
 				mainTitle="Cloud Computing Services"
 				description="At intekbit solutions pvt. ltd., we offer reliable cloud services designed to help businesses scale, secure their data, and improve efficiency. Our cloud solutions are tailored to meet the diverse needs of businesses, whether you're looking to migrate to the cloud, optimize your existing infrastructure, or develop cloud-native applications."
 				image="/Assets/internal-cloud-services.webp"
 			/>
-		</div>
 	);
 };
 
