244ab4baee9612146b5983c9772738861ebc76ff
sushil-kantaria <sushilkantaria22100@gmail.com>
2025-08-08 16:41:40 +0530

    hero section

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -18,7 +18,7 @@ const HeroSection = () => (
         data-aos-delay="500"
         data-aos-offset="0"
       >
-        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold drop-shadow-lg">
+        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold drop-shadow-lg">
           <Typewriter
             words={['WELCOME TO INTEKBIT SOLUTIONS PVT LTD']}
             loop={1}
@@ -26,7 +26,7 @@ const HeroSection = () => (
             typeSpeed={70}
           />
         </h1>
-        <p className="text-base sm:text-lg md:text-xl drop-shadow">
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl drop-shadow">
           <Typewriter
             words={['Driven by Technology, Powered by Trust.']}
             loop={1}
@@ -34,15 +34,6 @@ const HeroSection = () => (
             typeSpeed={70}
           />
         </p>
-        {/* <div className="relative w-fit">
-      <Link
-        to="/services"
-        className="peer flex items-center justify-center px-6 py-2 rounded-2xl transition-all duration-300 cursor-pointer relative z-10 text-white font-semibold text-center shadow-[0_2px_12px_0_rgba(30,58,138,0.18),0_1px_8px_0_rgba(136,19,55,0.13),0_1px_6px_0_rgba(202,138,4,0.10),0_1px_4px_0_rgba(6,78,59,0.10),inset_0_2px_16px_0_rgba(255,255,255,0.13),inset_0_0_12px_2px_rgba(255,255,255,0.10),inset_0_0_0_1px_rgba(255,255,255,0.07)]"
-      >
-        Learn More
-      </Link>
-      <div className="absolute inset-0 rounded-2xl opacity-0 peer-hover:opacity-100 shadow-[-4px_0_8px_2px_rgba(255,0,128,0.7),4px_0_8px_2px_rgba(0,98,255,0.7)]" />
-    </div> */}
       </div>
 
       {/* Spline on the Right (hidden on mobile) */}
